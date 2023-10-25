d33326510c607bd2a4e20a2dc59b69a0faa81bf6
RomanStashuk <romanstashuk1@gmail.com>
2023-10-25 16:07:28 +0300

    add listeners for inputs change

--- src/components/employyes-add-form/employyes-add-form.js
+++ src/components/employyes-add-form/employyes-add-form.js
@@ -9,8 +9,16 @@ class EmployeesAddForm extends Component {
       salary: '',
     };
   }
+
+  onValueChange = (evt) => {
+    this.setState({
+      [evt.target.name]: evt.target.value,
+    });
+  };
+
   render() {
     const { name, salary } = this.state;
+
     return (
       <div className="app-add-form">
         <h3>Добавьте нового сотрудника</h3>
@@ -19,11 +27,17 @@ class EmployeesAddForm extends Component {
             type="text"
             className="form-control new-post-label"
             placeholder="Как его зовут?"
+            name="name"
+            value={name}
+            onChange={this.onValueChange}
           />
           <input
             type="number"
             className="form-control new-post-label"
             placeholder="З/П в $?"
+            name="salary"
+            value={salary}
+            onChange={this.onValueChange}
           />
 
           <button type="submit" className="btn btn-outline-light">
